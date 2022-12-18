d3a00f8b1454e84e476ebf5253da4467991d8237
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-12-19 00:36:30 +0530

    update schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -19,6 +19,12 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+    "The track's complete desciption, can be in Markdown format"
+    description: String
+    "The number of times the track has been viewed"
+    numberOfViews: Int
+    "The Track's complete array of Modules"
+    modules: [Modules!]!
   }
 
   "Author of a complete Track or a Module"
@@ -28,6 +34,15 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
+  }
+
+  "A module is a single unit of tracking. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 
